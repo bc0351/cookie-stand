6f46bf0ef5b72b9d3236df6ff31163472501b90c
2six Dev <dev@twosixgroup.org>
2022-04-10 19:19:00 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//store min/max customers/hour, avg cookies/customer in object properties
+
+// use random number gnereator to random customers per hour
+
+// calculate and store amounts of cookies purchased per hour/location using avg cookies purchased and randome number of customers
+
+// stores results/location in seperate arrays -> property of location object
+
+// display values of each array as unordered lists in browser
+
+/* Expected output:
+
+Seattle
+
+6am: 16 cookies
+7am: 20 cookies
+8am: 35 cookies
+9am: 48 cookies
+10am: 56 cookies
+11am: 77 cookies
+12pm: 93 cookies
+1pm: 144 cookies
+2pm: 119 cookies
+3pm: 84 cookies
+4pm: 61 cookies
+5pm: 23 cookies
+6pm: 42 cookies
+7pm: 57 cookies
+Total: 875 cookies
+
+*/
+
+// display lists on sales.html
+
+// initial data:
+
+/* Location	Min / Cust	Max / Cust	Avg Cookie / Sale
+Seattle	23	65	6.3
+Tokyo	3	24	1.2
+Dubai	11	38	3.7
+Paris	20	38	2.3
+Lima	2	16	4.6 */
+
